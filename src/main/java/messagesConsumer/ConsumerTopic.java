d7c1990a0102a394e4a6e65468f09f60d5669d0d
lufisp <fernando@debian>
2016-11-29 16:49:50 -0500

    NewCommit

--- src/main/java/messagesConsumer/ConsumerTopic.java
+++ src/main/java/messagesConsumer/ConsumerTopic.java
@@ -3,33 +3,25 @@ package messagesConsumer;
 import java.util.Collections;
 import java.util.Properties;
 
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
-import org.springframework.stereotype.Component;
 
-import com.twitter.bijection.Injection;
-import com.twitter.bijection.avro.GenericAvroCodecs;
 
-@Component
 public class ConsumerTopic {
 	
-	private Properties kafkaProps = new Properties();
-	private KafkaConsumer<String, byte[]> consumer;
-	// private topic = "toHbase";
-	private static String topic = "atmOperations";
-
-	public static final String USER_SCHEMA = "{" + "\"type\":\"record\"," + "\"name\":\"atmRecord\"," + "\"fields\":["
-			+ "  { \"name\":\"id\", \"type\":\"string\" }," + "  { \"name\":\"operValue\", \"type\":\"int\" }" + "]}";
+	/*Closing the constructor*/
+	private ConsumerTopic(){}	
+	
+		
+	private KafkaConsumer<String, Integer> consumer; 
+	protected String topic;
+	protected Properties kafkaProps;
+	
 
 	public void startReading() {
-
-		final ConsumerTopic toHbaseConsumer = new ConsumerTopic();
-		toHbaseConsumer.configure("localhost:9092");
 
 		final Thread mainThread = Thread.currentThread();
 
@@ -39,7 +31,7 @@ public class ConsumerTopic {
 				System.out.println("Starting exit...");
 				// Note that shutdownhook runs in a separate thread, so the only
 				// thing we can safely do to a consumer is wake it up
-				toHbaseConsumer.consumer.wakeup();
+				consumer.wakeup();
 				try {
 					mainThread.join();
 				} catch (InterruptedException e) {
@@ -49,41 +41,42 @@ public class ConsumerTopic {
 		});
 
 		try {
-			toHbaseConsumer.consumer.subscribe(Collections.singletonList(topic));
-
-			Schema.Parser parser = new Schema.Parser();
-			Schema schema = parser.parse(USER_SCHEMA);
-			Injection<GenericRecord, byte[]> recordInjection = GenericAvroCodecs.toBinary(schema);
+			consumer = new KafkaConsumer<>(kafkaProps);
+			consumer.subscribe(Collections.singletonList(topic));	
 
 			// looping until ctrl-c, the shutdown hook will cleanup on exit
 			while (true) {
-				ConsumerRecords<String, byte[]> records = toHbaseConsumer.consumer.poll(1000);
+				ConsumerRecords<String, Integer> records = consumer.poll(1000);
 				System.out.println(System.currentTimeMillis() + "  --  waiting for data...");
-				for (ConsumerRecord<String, byte[]> avroRecord : records) {
-					System.out.printf("offset = %d\n", avroRecord.offset());
-					GenericRecord record = recordInjection.invert(avroRecord.value()).get();
-
-					System.out.println("id= " + record.get("id") + ", operValue= " + record.get("operValue"));
-
+				for (ConsumerRecord<String, Integer> record : records) {
+					System.out.printf("offset = %d\n", record.offset());
+					System.out.println("id= " + record.key() + ", operValue= " + record.value());
 				}
-				for (TopicPartition tp : toHbaseConsumer.consumer.assignment())
-					System.out.println("Committing offset at position:" + toHbaseConsumer.consumer.position(tp));
-				toHbaseConsumer.consumer.commitSync();
+				for (TopicPartition tp : consumer.assignment())
+					System.out.println("Committing offset at position:" + consumer.position(tp));
+				consumer.commitSync();
 			}
 		} catch (WakeupException e) {
 			// ignore for shutdown
 		} finally {
-			toHbaseConsumer.consumer.close();
+			consumer.close();
 			System.out.println("Closed consumer and we are done");
 		}
 	}
-
-	private void configure(String servers) {
-		kafkaProps.put("group.id", "gp1");
-		kafkaProps.put("bootstrap.servers", servers);
-		kafkaProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		kafkaProps.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
-		consumer = new KafkaConsumer<String, byte[]>(kafkaProps);
+	
+	public static ConsumerTopic ConsumerTopicBuilder(String brokerServer, String topic, String groupId){
+		ConsumerTopic consumerTopic = new ConsumerTopic();
+		consumerTopic.topic = topic;
+		consumerTopic.kafkaProps = new Properties();
+		consumerTopic.kafkaProps.put("group.id", groupId);
+		consumerTopic.kafkaProps.put("bootstrap.servers", brokerServer);
+		consumerTopic.kafkaProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		consumerTopic.kafkaProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		return consumerTopic;		
 	}
+
+	
+	
+	
 
 }
